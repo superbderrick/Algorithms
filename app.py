dde3871294cc3fe6e0830f183d6e82cf96eeaf13
derrick <derrick.kang@nexstreaming.com>
2018-01-19 18:04:05 +0900

    improved app

--- app.py
+++ app.py
@@ -5,30 +5,34 @@ from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
 
+
+# Create Flask.
 app = Flask(__name__)
 
 # Config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_PASSWORD'] = 'tkdrb134'
 app.config['MYSQL_DB'] = 'myflaskapp'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
 # init MYSQL
 mysql = MySQL(app)
 
+
 #Articles = Articles()
 
+
+# Routes.
 # Index
 @app.route('/')
 def index():
     return render_template('home.html')
 
-
 # About
 @app.route('/about')
 def about():
     return render_template('about.html')
-
 
 # Articles
 @app.route('/articles')
