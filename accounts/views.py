a8e9a652c90e075bb3749d9d513f291859c0874d
Derrick <kang.derrick@gmail.com>
2021-01-08 16:55:04 +0900

    fished chaper 03

--- accounts/views.py
+++ accounts/views.py
@@ -4,10 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-	return HttpResponse('home')
+	return render(request, 'accounts/dashboard.html')
 
 def products(request):
-	return HttpResponse('products')
+	return render(request, 'accounts/products.html')
 
 def customer(request):
-	return HttpResponse('customer')
+	return render(request, 'accounts/customer.html')
+
