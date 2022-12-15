229aa4da84bb7f4ed6f61b15714211bb2ef7acdd
Kisoon Kwon <moxie2ks@gmail.com>
2022-12-15 11:58:10 +0900

    Fixed unused code + explanation of comment

--- daniel/defense_game/defense_game.c
+++ daniel/defense_game/defense_game.c
@@ -13,37 +13,32 @@ int compare(void* first, void* second)
 
 int is_depense_ok(int n, int k, int mid, int* enemy)
 {
-    // 적 병사를 내림차순 정렬.
+    // 적 병사를 오름차순 정렬.
     int *sorted_enemy = (int*)malloc(sizeof(int)*(mid+1));
     memcpy(sorted_enemy, enemy, sizeof(int)*(mid+1));
     qsort(sorted_enemy, mid+1, sizeof(int), compare);
     
-    int remain = mid+1;
+    int remain_round = mid+1;
     int ret = 0;
     
     for (int i=0; i<=mid; i++)
     {
+        // 병사 차감시 남은 라운드를 카운트
         if (n>=sorted_enemy[i])
         {
             n-= sorted_enemy[i];
-            remain--;
-            continue;
+            remain_round--;
         }
-        
-        
-        
     }
     free(sorted_enemy);
     
-    // 남은 병사 숫자로 성공(1), 실패(0) 리턴
-    return k >= remain;
+    // 남은 라운드를 무적권으로 클리어 할 수 있으면(1), 없으면(0) 리턴
+    return k >= remain_round;
 }
 
 // enemy_len은 배열 enemy의 길이입니다.
 int solution(int n, int k, int enemy[], size_t enemy_len) {
     int answer = 0;
-    
-    int ret = 0;
     int begin = 0;
     int end = enemy_len;
     int mid = 0;
@@ -53,7 +48,7 @@ int solution(int n, int k, int enemy[], size_t enemy_len) {
     {
         mid = (begin + end) / 2;
         
-        // 성공 가능한지 여부를 체크하여 2진트리 인덱스 재설정
+        // 특정 라운드에서 성공 가능한지 여부를 체크하여 2진트리 인덱스 재설정
         if (is_depense_ok(n, k, mid, enemy))
         {
             begin = mid + 1;
@@ -63,9 +58,10 @@ int solution(int n, int k, int enemy[], size_t enemy_len) {
             end = mid;
         }
     }
+
     if (k>=enemy_len) // 무적권만으로 전체 라운드를 클리어할 경우
         answer = enemy_len;
-    else // 성공했던 round 리턴
+    else // 성공한 round 리턴
         answer = begin;
 
     return answer;
