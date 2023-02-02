2b0822895ccddf7ab10b67515b278293f632b12c
Kisoon Kwon <moxie2ks@gmail.com>
2023-02-02 16:33:06 +0900

    [Improvement] Replaced double vector with map - reduced time complexity.

--- daniel/tuple/tuple.cpp
+++ daniel/tuple/tuple.cpp
@@ -1,35 +1,31 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
-bool cmp(vector<int> a, vector<int>b) {
-    return a.size() < b.size();
+bool cmp(pair<int, int> a, pair<int, int>b) {
+    return a.second > b.second;
 }
 
 vector<int> solution(string s) {
     vector<int> answer;
     string value = "";
-    vector<vector<int>> tuples;
-    vector<int> tuple;
+    map<int, int> tuple_map;
     for(auto c : s) {
         switch(c) {
-            case '}': if (value.size()>0) {tuple.push_back(stoi(value)); tuples.push_back(tuple); tuple.clear(); value.clear();} break;
-            case ',': if (value.size()>0) {tuple.push_back(stoi(value)); value.clear();} break;
+            case '}':
+            case ',': if (value.size()>0) {tuple_map[stoi(value)]++; value.clear();} break;
             default : if (isdigit(c)) value += c; break;
         }
-        
     }
 
-    sort(tuples.begin(), tuples.end(), cmp);
-
-    for (auto tuple: tuples) {
-        for (auto i: tuple) {
-            if (find(answer.begin(), answer.end(), i) == answer.end()) {
-                answer.push_back(i);
-            }
-        }
+    vector<pair<int, int>> tuple_vector(tuple_map.begin(), tuple_map.end());
+    sort(tuple_vector.begin(), tuple_vector.end(), cmp);
+   
+    for (auto i: tuple_vector) {
+        answer.push_back(i.first);
     }
 
     return answer;
