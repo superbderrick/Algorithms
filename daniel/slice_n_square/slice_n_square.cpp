cf84c981a4f8827af2128e115c1b59f1e82a74fd
Kisoon Kwon <moxie2ks@gmail.com>
2023-02-03 13:32:40 +0900

    [Improvement] Simplify for getting value of target value, X.

--- daniel/slice_n_square/slice_n_square.cpp
+++ daniel/slice_n_square/slice_n_square.cpp
@@ -1,17 +1,14 @@
+#include <iostream>
 #include <string>
 #include <vector>
-#include <iostream>
+
 using namespace std;
 
 vector<int> solution(int n, long long left, long long right) {
     vector<int> answer;
-    int cycle = (left)/n + 1;
-    long long target = 0;
-    for(long long i=left+1; i<=right+1; i++) {
-        if (i%n == 0) { cycle++; target = n; }
-        else { target = i%n<=cycle ? cycle : i%n; }
 
-        answer.push_back(target);
+    for(long long i=left; i<=right; i++){
+        answer.push_back(max(i/n, i%n) + 1);
     }
 
     return answer;
