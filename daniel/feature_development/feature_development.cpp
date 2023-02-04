c776f19b1bc4d76009f4e80e5346cdab9e43784b
Kisoon Kwon <moxie2ks@gmail.com>
2023-02-04 23:32:38 +0900

    [Improvement] Use for operation once.

--- daniel/feature_development/feature_development.cpp
+++ daniel/feature_development/feature_development.cpp
@@ -1,30 +1,21 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    vector<int> required_days;
-    
-    for (int i=0; i<progresses.size(); i++) {
-        int rest = 100-progresses[i];
-        required_days.push_back(rest/speeds[i] + ((rest%speeds[i])>0?1:0));
-    }
+    int max = 0;
 
-    int count = 1;
-    int max = required_days[0];
-    for (int i=1; i<required_days.size(); i++) {
-        if (max < required_days[i]) {
-            answer.push_back(count);
-            max = required_days[i];
-            count = 1;
+    for (int i=0; i<progresses.size(); i++) {
+        int days = (100-progresses[i])/speeds[i] + (((100-progresses[i])%speeds[i])>0);
+        if (answer.empty() || max < days) {
+            answer.push_back(1);
+            max = days;
         } else {
-            count++;
+            answer.back()++;
         }
     }
-    answer.push_back(count);
 
     return answer;
 }
