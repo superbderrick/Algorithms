3b328d103bd60b54b4e78c88dec44e259a24b8c7
Kisoon Kwon <moxie2ks@gmail.com>
2023-04-28 17:44:13 +0900

    [Improved] Code refactorying.

--- daniel/compress/compress.cpp
+++ daniel/compress/compress.cpp
@@ -7,30 +7,25 @@ using namespace std;
 
 vector<int> solution(string msg) {
     vector<int> answer;
-    map<string, int> search_map;
+    map<string, int> dictionary;
     for (int i=0; i<26; i++) {
-        char temp_char = 'A'+i;
-        string temp_string;
-        temp_string += temp_char;
-        search_map[temp_string] = i+1;
+        dictionary[string(1, 'A'+i)] = i+1;
     }
-
+    
     string w = "";
     for (int i=0; i<msg.size(); i++) {
-        string c = "";      
-        if (msg[i+1]) c += msg[i+1];
         w += msg[i];
-
+        string c = msg[i+1] ? string(1,msg[i+1]) : "";
         if (c == "") {
-            answer.push_back(search_map[w]);
+            answer.push_back(dictionary[w]);
         } else {
-            if (search_map.find(w+c) == search_map.end()) {
-                search_map[w+c] = search_map.size()+1;
-                answer.push_back(search_map[w]);
+            if (dictionary.find(w+c) == dictionary.end()) {
+                dictionary[w+c] = dictionary.size()+1;
+                answer.push_back(dictionary[w]);
                 w.clear();
             }
         }
     }
-
+    
     return answer;
 }
