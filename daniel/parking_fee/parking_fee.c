994cb997e2ef6a081b407f3069cd50a0082245d9
Kisoon Kwon <moxie2ks@gmail.com>
2023-06-04 11:08:18 +0900

    [Improved] Code refactorying.

--- daniel/parking_fee/parking_fee.c
+++ daniel/parking_fee/parking_fee.c
@@ -12,7 +12,7 @@ int found_car_number(int* car_numbers, size_t car_numbers_size, int number_to_fi
         if (car_numbers[j] == number_to_find)
             return 1;
     }
-
+    
     return 0;
 }
 
@@ -22,43 +22,47 @@ int found_car_number(int* car_numbers, size_t car_numbers_size, int number_to_fi
 int* solution(int fees[], size_t fees_len, const char* records[], size_t records_len) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     int* answer;
-
+    
     int* car_numbers;
     size_t car_numbers_length = 1;
     car_numbers = (int*)malloc(car_numbers_length * sizeof(int));
-    car_numbers[0] = -1;
+
+    // variables for parsing records.
     int hours, mins, car_num;
     char status[4];
-
-    int car_numbers_index = 0;
+    
+    // Get car_number's list
     for (int i=0; i<records_len; i++) {
         sscanf(records[i], "%d:%d %d %s", &hours, &mins, &car_num, status);
-        if (!found_car_number(car_numbers, car_numbers_length, car_num)) {
-            if (i==0) {
-                car_numbers[0] = car_num;
-            } else {
-                car_numbers = (int*)realloc(car_numbers, (++car_numbers_length)*sizeof(int));
-                car_numbers[car_numbers_length-1] = car_num;
-            }
+        if (i==0) {
+            car_numbers[0] = car_num;
+        } else if (!found_car_number(car_numbers, car_numbers_length, car_num)) {
+            car_numbers = (int*)realloc(car_numbers, (++car_numbers_length)*sizeof(int));
+            car_numbers[car_numbers_length-1] = car_num;
         }
     }
-
+    
+    // Sort car_numbers.
+    qsort(car_numbers, car_numbers_length, sizeof(int), compare);
+    
+    // Allocate answer memory by car_number's length
     answer = (int*)malloc(car_numbers_length*sizeof(int));
     memset(answer, 0, car_numbers_length*sizeof(int));
 
-    qsort(car_numbers, car_numbers_length, sizeof(int), compare);
-
+    // Check car_numbers each
     for (int j=0; j<car_numbers_length; j++) {
-        int begin_time = 0;
-        int elapsed_time = 0;
+        int begin_time = 0, elapsed_time = 0;
         int paired = 0;
+        // Parse records
         for (int i=0; i<records_len; i++) {
             sscanf(records[i], "%d:%d %d %s", &hours, &mins, &car_num, status);
             if (car_numbers[j] == car_num) {
+                // Set begin_time when status is "IN"
                 if (strncmp(status, "IN", 3) == 0) {
                     begin_time = hours*60 + mins;
                     paired = 0;
                 }
+                // Set elapsed_time when status is "OUT"
                 if (strncmp(status, "OUT", 4) == 0) {
                     elapsed_time += (hours*60 + mins) - begin_time;
                     paired = 1;
@@ -66,14 +70,20 @@ int* solution(int fees[], size_t fees_len, const char* records[], size_t records
             }
         }
 
-        if (paired == 0) {
+        // Set elapsed_time when there's no "OUT" status.
+        if (!paired) {
             elapsed_time += (23*60 + 59) - begin_time;
-            begin_time = 0;
         }
+        
+        // Get unit_time by elapsed_time.
+        int unit_time = ceil((elapsed_time - fees[0]) / (double)fees[2]);
 
-        int elapsed_fee = ceil(((float)(elapsed_time - fees[0]) / (float)fees[2]));
-        answer[j] += fees[1] + (elapsed_fee <= 0 ? 0 : elapsed_fee) * fees[3];
-    }   
-
+        // Get total fee and set answer.
+        answer[j] += fees[1] + (unit_time > 0 ? unit_time : 0) * fees[3];
+    }
+    
+    // Free memory.
+    free(car_numbers);
+    
     return answer;
 }
